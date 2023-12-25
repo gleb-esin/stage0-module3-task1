7923fab600ed2d369e2495a1ca428f2e21b7f1a6
Gleb <obuis@gmail.com>
2023-12-25 22:05:45 +0300

    Initial commit

--- src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/UpdatedApp.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class UpdatedApp {
     public static void main(String[] args) {
-        System.out.println("fixMe");
+        System.out.print("I'm updated" + System.lineSeparator() + "can be compiled. And can write to console");
     }
 }
